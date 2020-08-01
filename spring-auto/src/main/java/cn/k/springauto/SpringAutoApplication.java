14f4712bb193d27c8edc57313f1e20326ca8db8f
googlebrain@163.com <googlebrain@163.com>
2020-08-01 19:56:43 +0800

    springboot关键技术讲解

--- spring-auto/src/main/java/cn/k/springauto/SpringAutoApplication.java
+++ spring-auto/src/main/java/cn/k/springauto/SpringAutoApplication.java
@@ -3,16 +3,25 @@ package cn.k.springauto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.annotation.Order;
+import org.springframework.util.StopWatch;
 
 @SpringBootApplication
 public class SpringAutoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws  Exception{
         ConfigurableApplicationContext run = SpringApplication.run(SpringAutoApplication.class, args);
         String[] beanDefinitionNames = run.getBeanDefinitionNames();
         for(String a:beanDefinitionNames){
             System.out.println(a);
         }
+
+        StopWatch my = new StopWatch("aaaa");
+        my.start("T1");
+        Thread.sleep(3000L);
+        my.stop();
+
+        System.out.println(my.prettyPrint());
 
     }
 
---
+++ spring-auto/src/main/java/cn/k/springauto/startup/FirstCommandLineRunner.java
@@ -0,0 +1,17 @@
+package cn.k.springauto.startup;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * 启动加载器，框架启动完了，立即执行的。
+ */
+@Component
+@Order(2)
+public class FirstCommandLineRunner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("this is my first runner&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+    }
+}
